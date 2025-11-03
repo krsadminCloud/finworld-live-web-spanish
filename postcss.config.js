e7f927d7df0f5387854107a7edb8c416a684b8e0
krsadminCloud <kferdous1977@outlook.com>
2025-11-02 22:36:10 -0500

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,7 @@
-// postcss.config.js - for Azure Static Web App build (CommonJS format)
+// postcss.config.js - for Azure build
+const tailwindcss = require("tailwindcss");
+const autoprefixer = require("autoprefixer");
+
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [tailwindcss("./tailwind.config.js"), autoprefixer],
 };
